629c6b04865782bc6c8a3d6dfb45776675c7100d
alex1gtp <alex1gtp@gmail.com>
2017-01-26 20:13:45 +0200

    NavBar - login sections
    include section in navbar for login with more atributes

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -26,16 +26,38 @@
         <li><a href="/page/inscriete">Inscrie-te</a></li>
         <li><a href="/page/contact">Contact</a></li>
         @if(\Illuminate\Support\Facades\Auth::check())
+        <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">USERNAME
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu" style="background-color: #004326">
           <li>
-            <a href="{{ url('/logout') }}"
-               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+            
+             <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              Profil
+            </a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </li>
+          <li>
+            
+             <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              Echipa
+            </a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </li>
+          <li>
+            
+             <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
               Logout
             </a>
             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
               {{ csrf_field() }}
             </form>
           </li>
+         </ul> 
         @else
           <li><a href="#" data-toggle="modal" data-target="#loginModal">logIn</a></li>
         @endif
