2e9c299b39ed3340055ab5a7cb50a05f925a243d
alex1gtp <alex1gtp@gmail.com>
2017-01-26 13:44:42 +0200

    Refactory

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,6 +4,9 @@
   <!--META-->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--JQuery-->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
   <!--Bootstrap CSS-->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
    <!--FONTS-->
@@ -12,10 +15,10 @@
  
   <!--JS-->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
    <script src="js/inscriere.js"></script>
   <script src="js/particles.js" type="text/javascript"></script>
-  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+
    <script src='http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
    <script src="js/index.js" type="text/javascript"></script>
   <!--STYLES-->
